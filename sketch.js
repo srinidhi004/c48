7a6d8f8392dfb8ca4136256dcfba86a844567dbf
srinidhi004 <62704851+srinidhi004@users.noreply.github.com>
2020-08-16 12:54:04 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,437 @@
+var runner,runnerImg;
+var building1,building2,building3,building4,building5;
+var building;
+var buildingbg, buildingbgimg;
+var farm,farmImg;
+var parachute,parachuteImg;
+var mountain, mountainImg,mountI;
+var c1,c1img,c2,c2img;
+var coin,coinImg;
+var cycling,cyclingImg;
+var desertImg,desert;
+var dig,digImg;
+var farm,farmImg;
+var farm,farmImg, paper,paperImg;
+var treasureChest,stadium;
+var treasureChestimg,stadiumimg;
+var buildingGroup;
+var steps,stepsimg;
+var ig, ig2, ig3, ig4;
+var PLAY=1;
+var END= 0;
+var gamestate= PLAY;
+var LEVEL1;
+var LEVEL2;
+var LEVEL3;
+var level1start=2;
+var level1end=3;
+var level2start=4;
+var level2end=5;
+var level3start=6;
+var level3end=7;
+var level=LEVEL1;
+var coingroup, buildingGroup;
+var retry, retryimg;
+var count,coin;
+var farmbg;
+var nextlevel;
+var plant, cactusimg, cactusgroup;
+var rock, rockimg;
+var rockgroup,igt;
+var flagimg, flag;
+var digbutton, digbuttonimg;
+var scroll, scrollimg;
+var life1, lifeimg, life2,life3, life4, life5;
+var life=5;
+var backgroundSound;
+function preload(){
+  runnerImg=loadAnimation("Images/runner1.png","Images/runner3.png","Images/runner2.png","Images/runner4.png");
+  runnerJump= loadImage("Images/runner2.png");
+  runnerfall= loadImage("Images/runner3.png");
+  digImg=loadAnimation("Images/dig1.png","Images/dig2.png","Images/dig3.png");
+  building1= loadImage("Images/building1.png");
+  building2= loadImage("Images/building2.png");
+  building3= loadImage("Images/building3.png");
+  building4= loadImage("Images/building4.png");
+  building5= loadImage("Images/building5.png");
+  buildingbgimg= loadImage("Images/buildingBG.png");
+  farmImg=loadImage("Images/farm.jpg");
+  parachuteImg=loadImage("Images/parachute.png");
+  mountainImg=loadImage("Images/mountainClimb.gif");
+  mountI= loadImage("Images/mountain.jpg");
+  c1img=loadImage("Images/c1.jpg");
+  c2img=loadImage("Images/c2.png");
+  coinImg=loadImage("Images/coin.png");
+  cyclingImg= loadAnimation("Images/cycling.gif");
+  desertImg= loadImage("Images/desert.jpg");
+  digImg= loadAnimation("Images/dig.gif");
+  farmImg= loadImage("Images/farm.jpg");
+  paperImg= loadImage("Images/paper.png");
+  treasureChestimg=loadImage("Images/treasureChest.jpg");
+  stadiumimg= loadImage("Images/stadium.jpg");
+  stepsimg= loadImage("Images/steps.png");
+  retryimg= loadImage("Images/retry.png");
+  nextlevelimg= loadImage ("Images/nextlevel.png");
+  rockimg= loadImage("Images/rock.png");
+  cactusimg= loadImage("Images/cactus.png");
+  digbuttonimg= loadImage("Images/digbutton.png");
+  flagimg= loadImage("Images/flag.png");
+  scrollimg= loadImage("Images/scroll.png");
+  lifeimg= loadImage("Images/life.png");
+  backgroundSound= loadSound("sounds/backgroundFull.mp3");
+}
+
+
+
+function setup() {
+  createCanvas(1300,600);
+  buildingbg= createSprite(650, 300, 2500, 1000);
+  buildingbg.addImage(buildingbgimg);
+  farmbg= createSprite(650, 300, 2500, 1000);
+  farmbg.addImage(farmImg);
+  desertbg= createSprite(650, 300, 2500, 1000);
+  desertbg.addImage(desertImg);
+  farmbg.visible=false;
+  desertbg.visible=false;
+  buildingbg.scale= 3.5;
+  farmbg.scale=3.2;
+  desertbg.scale=3.2;
+  runner= createSprite(130, 100,30,50);
+  runner.addAnimation("running",runnerImg);
+  runner.scale=0.4;
+  dig= createSprite(650, 300, 2500, 1000);
+  dig.addAnimation("digging",digImg);
+  dig.visible=false;
+  runner.setCollider("circle",0,0,100);
+  treasureChest= createSprite(1000,300,40,30);
+  treasureChest.addImage(treasureChestimg);
+  treasureChest.visible=false;
+  buildingGroup= createGroup();
+  coingroup= createGroup();
+  rockgroup= createGroup();
+  cactusgroup= createGroup();
+  ig= createSprite(100,140,150,20);
+  ig.visible=false;
+  ig2= createSprite(550,530,1500,20);
+  ig2.visible=false; 
+  //ig4= createSprite(650,450 ,1300,20);
+  //ig4.visible=false;
+  retry= createSprite(700, 300);
+  retry.addImage(retryimg);
+  retry.scale=0.7;
+  retry.visible= false;
+  nextlevel=createSprite(700,500);
+  nextlevel.addImage(nextlevelimg);
+  nextlevel.visible=false;
+  igt= createSprite(0,300,20,600);
+  igt.visible=false;
+  igt.velocityX=0;
+  igt.velocityY=0;
+  igm= createSprite(100,500,150,20);
+  igm.visible= false;
+  scroll=createSprite(600,200,30,50);
+  scroll.addImage(scrollimg);
+  scroll.visible=false;
+  life1= createSprite(1100,50,30,30);
+  life1.addImage(lifeimg);
+  life1.scale=0.1;
+  life2=createSprite(1150,50,30,30);
+  life2.addImage(lifeimg);
+  life2.scale=0.1;
+  life3=createSprite(1050,50,30,30);
+  life3.addImage(lifeimg);
+  life3.scale=0.1;
+  life4=createSprite(1000,50,30,30);
+  life4.addImage(lifeimg);
+  life4.scale=0.1;
+  life5=createSprite(950,50,30,30);
+  life5.addImage(lifeimg);
+  life5.scale=0.1;
+  
+}
+function draw() {
+ // console.log(frameCount);
+  backgroundSound.play();
+  background("white"); 
+  igt.collide(runner);
+  //runner.collide(igm);
+  runner.velocityX=0;
+  if(frameCount===1){
+    alert('Use space key to make the runner jump.');
+  }
+  if(gamestate===level1start|| gamestate===level2start|| gamestate===level3start){
+    gamestate=PLAY;
+  }
+  if(life===4){
+    life5.visible=false;
+    gamestate=PLAY;
+  }
+  if(life===3){
+    life5.visible=false;
+    life4.visible=false;
+    gamestate=PLAY;
+  }
+  if(life===2){
+    life5.visible=false;
+    life4.visible=false;
+    life3.visible=false;
+    gamestate=PLAY;
+  }
+  if(life===1){
+    life5.visible=false;
+    life4.visible=false;
+    life3.visible=false;
+    life1.visible=false;
+    gamestate=PLAY;
+  }
+  if(gamestate===PLAY)  { 
+    if(keyDown("space")&& runner.y > 150){
+      runner.velocityY=-10 ;
+      runner.addImage(runnerJump);
+    }
+   
+    runner.velocityY=runner.velocityY+0.8;
+    if(frameCount>0 && frameCount<999){
+      buildingbg.visible=true;
+      level=LEVEL1;
+      buildings(); 
+      buildingGroup.setVisibleEach(true);
+      rockgroup.setVisibleEach(false);
+    }
+    
+    if(frameCount<285 || frameCount===285){
+      runner.collide(ig);
+      coingroup.setVisibilityEach= false;
+    }
+     else{
+       runner.collide(buildingGroup);
+       coingroup.setVisibilityEach= true;
+     }
+    
+    if(frameCount>0 && frameCount<999){
+      buildingbg.visible=true;
+      count= Math.round(World.frameCount/5);
+      level=LEVEL1;
+      coingroup.setVisibleEach(false);
+       buildingbg.velocityX=-(4+Math.round(World.frameCount/100));
+      if(buildingbg.x<0){
+        buildingbg.x= buildingbg.width/2;
+      }  
+      buildingbg.cvelocityX=-(4+Math.round(World.frameCount/200));
+      if(buildingbg.x<0){
+        buildingbg.x= buildingbg.width/2;
+        rockgroup.setVisibilityEach(false);
+        cactusgroup.setVisibleEach(false);
+      }
+      if(mousePressedOver(retry)){
+        reset();
+      }
+      if( ig2.isTouching(runner)){
+        life-=1;
+        gamestate=PLAY;
+      }
+        if(life===0){
+          buildingbg.velocityX = 0;
+          // console.log(buildingbg.velocityX);
+           gamestate= END;
+           runner.addAnimation(runnerfall);
+           retry.visible=true;
+           buildingbg.velocityX = 0;
+           buildingbg.velocityY = 0;
+           
+           buildingGroup.setLifetimeEach(-1);
+           runner.velocityY= 0;
+           buildingGroup.setVelocityXEach(0);
+           buildingGroup.setVelocityYEach(0);     
+           runner.changeAnimation(runnerfall);
+           count= count;
+        
+       }
+     }
+    if(frameCount>999 || frameCount===1000 && frameCount<1001){
+      gamestate= level1end;
+     // alert('LEVEL2 : Use space key to make the runner jump.Collect coins to increase your score');
+    }
+      if(gamestate===level1end){
+      
+       //nextlevel.visible=true;
+       gamestate=level2start;
+  }
+
+  else{
+   // nextlevel.visible=false;
+  }
+  if(frameCount>1000 && frameCount<2500){
+    gamestate= level2start;
+    runner.velocityX=0;
+  }
+ /* console.log(gamestate);
+  console.log(level);*/
+   if(gamestate===level2start){
+   // gamestate=level2start
+   farmbg.visible=true;
+    
+    //runner.velocityY=0;
+    runner.collide(igt);
+    retry.visible=false;
+    buildingbg.visible=false;
+    if(farmbg.x<0){
+      farmbg.x= farmbg.width/2;
+    }  
+    runner.collide(ig2);
+    coins();
+    rocks();
+    farmbg.velocityX=-(4+Math.round(World.frameCount/200));
+    if(runner.isTouching(coingroup)){
+      count= count+1;
+      coingroup.visibility=false;
+    }  
+    
+    if(rockgroup.isTouching(runner)){
+      gamestate=END;
+    }
+    if(frameCount>2500 || frameCount<2502 ){
+      gameState=level2end;
+     // nextlevel.visible=true;
+    }
+    else{
+      //nextlevel.visible=false;
+    }
+   // console.log(frameCount);
+    }   
+    if(frameCount>2500 || frameCount===4000 && frameCount<4001){
+      desertbg.visible=true;
+      gameState=level3start;
+      level= LEVEL3;
+      desertbg.visible=true;
+      retry.visible=false;
+      buildingbg.visible=false;
+      farmbg.visible=false;
+      cactusgroup.setVisibleEach(true);
+      rockgroup.setVisibleEach(false);
+      if(desertbg.x<0){
+        desertbg.x= desertbg.width/2;
+      }  
+      cactus();
+      desertbg.velocityX=-(4+Math.round(World.frameCount/250));
+      count= Math.round(World.frameCount/5);
+      if(cactusgroup.isTouching(runner)){
+        gamestate=END;
+      }
+      if(cactusgroup.isTouching(runner) && frameCount>3500){
+        gamestate=END;
+        scroll.visible=true;
+        retry.visible=false;
+      }
+    }
+   
+  
+   if(gamestate===END){
+      if(buildingGroup.isTouching(runner)|| rockgroup.isTouching(runner)|| runner.x<0 || cactusgroup.isTouching(runner)){
+        // buildingGroup.collide(runner);
+         buildingbg.velocityX = 0;
+         buildingbg.velocityY = 0;
+         farmbg.velocityX = 0;
+         farmbg.velocityY = 0;
+         buildingGroup.setLifetimeEach(-1);
+         coingroup.setLifetimeEach(-1);
+         rockgroup.setLifetimeEach(-1);
+         runner.velocityY= 0;
+         buildingGroup.setVelocityXEach(0);
+         buildingGroup.setVelocityYEach(0); 
+         coingroup.setVelocityXEach(0);
+         coingroup.setVelocityYEach(0);   
+         rockgroup.setVelocityXEach(0);
+         rockgroup.setVelocityYEach(0);
+         runner.addAnimation(runnerfall);
+         retry.visible=true;
+         frameCount=0;
+        
+          }
+      }
+      if(mousePressedOver(retry)){
+        gameState=PLAY;
+         runner.collide(ig);
+        reset();
+      }
+  drawSprites();
+  text("Score 0"+count,1200,50);
+  fill(0,0,0);
+  //fontSize(10);
+}
+}
+function buildings(){
+if(frameCount % 100 === 0){
+  building= createSprite(1300,500 ,50,200);
+   building.velocityX=-6;
+   //building.debug=true;
+   buildingGroup.add(building);
+   var rand = Math.round (random(1,5));
+   //steps.visible=false;
+   //console.log(rand);
+   switch (rand){
+    case 1 : building.addImage(building1);
+    break;
+    case 2 : building.addImage(building2);
+    break;
+    case 3 : building.addImage(building3);
+    break;
+    case 4 : building.addImage(building4);
+    break;
+    case 5 : building.addImage(building5);
+    break;
+    default : break;
+   }
+   building.depth=1;
+ }
+ 
+}
+
+function coins(){
+  if(frameCount % 60 === 0  || frameCount % 80 === 0){
+    var rand= Math.round(random(1200, 1300));
+    var rand1= Math.round(random(100,300));
+    coin= createSprite(rand,rand1 ,50,200);
+    coin.addImage(coinImg);
+     coin.velocityX=-8;
+     coin.scale=0.1;
+     coingroup.add(coin);
+   }
+   
+  }
+
+function reset(){
+  //gamestate= PLAY;
+  retry.visible= false;
+ /* if(frameCount<285 || frameCount===285){
+    runner.collide(ig); 
+   }*/
+  
+   runner.x=90;
+  buildingGroup.destroyEach();
+  buildingbg.velocityX=-(4+Math.round(World.frameCount/100));
+  count=0;
+ }
+ function rocks(){
+  if(frameCount % 80 === 0){
+    rock= createSprite(1300,550,50,200);
+    rock.addImage(rockimg);
+    rock.velocityX=-10;
+    rock.scale=0.1;
+    rockgroup.add(rock);
+    runner.depth= rock.depth;
+   }
+   
+  }
+
+  function cactus(){
+    if(frameCount % 80 === 0){
+      plant= createSprite(1300,500,50,200);
+      plant.addImage(cactusimg);
+      plant.velocityX=-8;
+      plant.scale=0.3;
+      cactusgroup.add(plant);
+      runner.depth= plant.depth;
+    }
+  }
+
